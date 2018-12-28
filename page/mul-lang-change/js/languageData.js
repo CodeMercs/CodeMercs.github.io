53ea1ec91deadcc08fcb1c8e61b618b67d4f668e
kancheng <zxdfgcv@gmail.com>
2018-12-28 14:37:37 +0800

    Upload JS

---
+++ page/mul-lang-change/js/languageData.js
@@ -0,0 +1,36 @@
+﻿/* All Language Data */
+
+/* Traditional Chinese - Taiwan */
+L_taiwan = new Array(); 
+L_taiwan[0] = "這是範例網頁，您可以在這裡預覽及測試網站翻譯工具。"; 
+L_taiwan[1] = "試試看變更以上設定並且按一下[預覽並試用！]按鈕看看效果。"; 
+L_taiwan[2] = "舉例來說，您選取要翻譯的語言後，下拉式選單中就會反應這個變更。"; 
+L_taiwan[3] = "好好享受吧！"; 
+
+/* Simplified Chinese */
+L_china = new Array(); 
+L_china[0] = "这是范例网页，您可以在这里预览及测试网站翻译工具。"; 
+L_china[1] = "试试看变更以上设定并且按一下[预览并试用！ ] 按钮看看效果。"; 
+L_china[2] = "举例来说，您选取要翻译的语言后，下拉式选单中就会反应这个变更。"; 
+L_china[3] = "好好享受吧！"; 
+ 
+/* Japan */ 
+L_japan = new Array(); 
+L_japan[0] = "これはサンプルページは、プレビューできますし、テストのWeb翻訳ツールです。"; 
+L_japan[1] = "&nbsp;[プレビュー]を上記の設定をクリックして変更して試してみてください！ ]ボタンは、効果を確認する。"; 
+L_japan[2] = "&nbsp;たとえば、あなたは、ドロップダウンメニューがこの変化に対応する翻訳する言語を選択します。"; 
+L_japan[3] = "&nbsp;をお楽しみください！"; 
+ 
+/* Korean */
+L_korean = new Array(); 
+L_korean[0] = "이것은 샘플 페이지, 당신은 할 수 미리보기 및 테스트 웹 번역 도구입니다."; 
+L_korean[1] = "[미리보기를 클릭 위의 설정을 변경하고 시도하십시오! ] 버튼을 클릭하면 효과를 볼 수 있습니다."; 
+L_korean[2] = "예를 들어, 당신은, 드롭 다운 메뉴에서 해당 변경에 응답할 것입니다 번역될 언어를 선택하십시오."; 
+L_korean[3] = "즐거운 시간 되십시오!"; 
+
+/* Vietnam */
+L_vietnam = new Array(); 
+L_vietnam[0] = "Đ&acirc;y l&agrave; một trang mẫu, nơi bạn c&oacute; thể xem trước v&agrave; dịch thử nghiệm c&aacute;c c&ocirc;ng cụ web."; 
+L_vietnam[1] = "H&atilde;y thử thay đổi c&aacute;c thiết lập ở tr&ecirc;n v&agrave;o [Xem v&agrave; thử! ] Button để xem hiệu quả."; 
+L_vietnam[2] = "V&iacute; dụ, bạn chọn ng&ocirc;n ngữ cần dịch, c&aacute;c tr&igrave;nh đơn thả xuống sẽ đ&aacute;p ứng với thay đổi n&agrave;y."; 
+L_vietnam[3] = "Thưởng thức!"; 
