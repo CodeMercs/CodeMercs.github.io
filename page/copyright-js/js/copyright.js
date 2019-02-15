d88937db6d2ebefa7c5c1cd0539fb6023c20d9c4
kancheng <zxdfgcv@gmail.com>
2019-02-15 11:12:43 +0800

    edit

---
+++ page/copyright-js/js/copyright.js
@@ -0,0 +1,13 @@
+﻿	/* Copyright JS */
+
+	var SystemDate = new Date();
+	var SystemDateByYear = SystemDate.getFullYear();
+
+	var CopyrightTag = document.getElementById('Copyright');
+
+	var CopyrightHead = "Copyright &copy ";
+	var CopyrightYear = SystemDateByYear;
+	var CopyrightName = "[公司名稱]";
+	var CopyrightTail = "[公司類型], Inc. All Rights Reserved.";
+
+	CopyrightTag.innerHTML = CopyrightHead + CopyrightYear + " " + CopyrightName + " " + CopyrightTail;
